a84f0d52f2ef94376cb0bdbc133a0ddfaa58dded
Ashish Jain <ashkjain0@gmail.com>
2024-02-10 10:24:11 -0700

    Added file called test2

---
+++ lesson01/test2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+myList = ['a','b','c']
+myBool = True
+myNone = None
+
+def myFunc():
+    print("hello")
+
+print(type(myList)) 
+print(type(myBool)) 
+print(type(myNone)) 
+print(type(myFunc)) 
+
+
+this_type = type(myList)
+print(type(this_type))
+
+print; print
