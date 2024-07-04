9dbda375af34108e9f56f0dbb6d51c61a9bfa976
Ashish Jain <ashishjain99@yahoo.com>
2024-07-04 13:05:59 -0600

    Finished First Module

---
+++ modules/main-packages.py
@@ -0,0 +1,6 @@
+import project_modules.communication.com as pcc # You can use this or you can directly import the function
+pcc.my_fun()
+from project_modules.communication.com import my_fun
+my_fun()
+
+
